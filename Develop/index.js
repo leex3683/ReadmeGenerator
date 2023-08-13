d598c1d427621ef76f64434c03d6794e02a79271
unknown <leex3683@umn.edu>
2023-08-12 20:01:27 -0500

    inquirer filled out, fs working

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const md = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.log(err) : console.log('Success')
+    );
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Title',
+            name: 'Title',
+        },
+        {
+            type: 'input',
+            message: 'Description',
+            name: 'Description',
+        },
+        {
+            type: 'input',
+            message: 'Table of Contents',
+            name: 'Table of Contents',
+        },
+        {
+            type: 'input',
+            message: 'Installation',
+            name: 'Installation',
+        },
+        {
+            type: 'input',
+            message: 'Usage',
+            name: 'Usage',
+        },
+        {
+            type: 'list',
+            name: 'licence',
+            message: 'licence',
+            choices: ['Apache 2.0', 'Boost', 'BSD']
+        },
+        {
+            type: 'input',
+            message: 'Contributing',
+            name: 'Contributing',
+        },
+        {
+            type: 'input',
+            message: 'Tests',
+            name: 'Tests',
+        },       
+        {
+            type: 'input',
+            message: 'Questions',
+            name: 'Questions',
+        },
+    ])
+    .then((response) =>
+    // md.generateMarkdown(response)
+    writeToFile(`${response.Title}.md`, md(response))
+    );
+
+}
 
 // Function call to initialize app
 init();
